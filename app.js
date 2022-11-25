98d7043dfcfd4a214e2510e4526e519a2be0d91b
Mickael Gonzales <mickael.gonzales@hotmail.com>
2022-11-25 12:18:45 +0100

    btn confirm, empêche valeur max < valeur actuelle

--- app.js
+++ app.js
@@ -3,21 +3,38 @@ let preview = document.querySelector('#preview');
 let meter = document.querySelector('#meter');
 let maxChars = document.querySelector('#maxChars');
 let compteur = document.querySelector('.compteur');
+let confirm = document.querySelector('#confirm');
+let save = 200;
 
-let nbChars;
 
-maxChars.addEventListener('input', function () {
-  meter.max = maxChars.value;
-  // compteur.innerText = `${editor.value.length} / ${meter.max}`;
+compteur.innerText = `0 / ${meter.max}`;
+
+confirm.addEventListener('click', function () {
+  if(maxChars.value < editor.value.length){
+    maxChars.value = save;
+    return alert('Attention, la valeur maximale de caractères autorisés ne peut pas' + 
+      'être inférieure au nombre de caractères actuellement utilisés !');
+  }
+  else{
+    meter.max = maxChars.value;
+    compteur.innerText = `${editor.value.length} / ${meter.max}`;
+    meter.low = meter.max / 2;
+    meter.high = meter.max / 1.25;
+    save = maxChars.value;
+  }
 });
 
 editor.addEventListener('input', function () {
-  preview.innerHTML = editor.value;
-  nbChars = editor.value.length;
-  meter.value = nbChars;
-  compteur.innerText = `${nbChars} / ${meter.max}`;
+  if(editor.value.length <= meter.max){
+    preview.innerHTML = editor.value;
+    meter.value = editor.value.length;
+    compteur.innerText = `${meter.value} / ${meter.max}`;
+  }
+  else{
+    return alert("Vous avez dépassé le nombre maximal de caractères !")
+  }
 
-  let toLimit = meter.max - nbChars;
-  console.log(`${toLimit}/${meter.max}`);
+  /*let toLimit = meter.max - nbChars;
+  console.log(`${toLimit}/${meter.max}`);*/
 });
 /* test*/
